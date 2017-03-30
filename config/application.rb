3e317bff7ac9d62fc7574ed64c6e607c0f614c8a
Rashmi Aroskar Gupta <raroskar@mdsol.com>
2017-03-30 14:09:30 -0400

    updated

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "sprockets/railtie" if Rails.env.development?
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Cirdan
+module Swagger_UI_RAILS_HOST
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
